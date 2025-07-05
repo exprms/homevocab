c9b5a058a8a15b6740a28d99cdef49fa355e0024
Bernd <bernd@192.168.1.159>
2025-07-05 09:35:25 +0200

    feat: add sheetname

--- vocab_manager/ods2csv.py
+++ vocab_manager/ods2csv.py
@@ -9,23 +9,36 @@ Created on Sun Mar 23 07:12:53 2025
 import os
 import pandas as pd
 from dotenv import load_dotenv
+import argparse
 import util.custom_logger as cl
-load_dotenv()
 
 
-def convert_ods_to_csv(input_file: str, output_file: str, check_col: str):
+
+def convert_ods_to_csv(
+        input_file: str, 
+        output_file: str, 
+        check_col: str = "", 
+        sheet_name: str = ""
+        ):
+
     # Read the .ods file
-    df = pd.read_excel(input_file, engine='odf')
-
-    cl.logger.info(f"check for duplicates in column {CHECK_FOR_DUPLICATES}")
-    duplicates = df[df.duplicated([check_col], keep=False)].sort_values(by=check_col, ascending=True)
-
-    if not duplicates.empty:
-        cl.logger.warning("Found duplicates: -----------------------------------")
-        print(duplicates[['id', check_col]])
-        cl.logger.warning("-----------------------------------------------------")
+    if sheet_name == "":
+        df = pd.read_excel(input_file, engine='odf')
+        cl.logger.info("load first sheet")
     else:
-        cl.logger.info("No duplicates found")
+        df = pd.read_excel(input_file, engine='odf', sheet_name=sheet_name)
+        cl.logger.info(f"load sheet: '{sheet_name}'")
+
+    if check_col != "":
+        cl.logger.info(f"check for duplicates in column {check_col}")
+        duplicates = df[df.duplicated([check_col], keep=False)].sort_values(by=check_col, ascending=True)
+
+        if not duplicates.empty:
+            cl.logger.warning("Found duplicates: -----------------------------------")
+            print(duplicates[['id', check_col]])
+            cl.logger.warning("-----------------------------------------------------")
+        else:
+            cl.logger.info("No duplicates found")
 
     # Write the DataFrame to a .csv file
     df.to_csv(output_file, index=False, sep=';')
@@ -33,9 +46,30 @@ def convert_ods_to_csv(input_file: str, output_file: str, check_col: str):
     cl.logger.info(f"Created csv file: '{output_file}'")
 
 if __name__ == "__main__":
+
+    # get env variables
+    load_dotenv()
+    
+    # Create the parameter parser
+    parser = argparse.ArgumentParser()
+
+    # Add arguments
+    #required
+    parser.add_argument("sheetname", help="name of ods sheet to parse")
+
+    # optional
+    parser.add_argument("--chk", type=str, help="which column to check for duplicates", default="")
+
+    args = parser.parse_args()
+    
     # Example usage
     input_file = os.getenv('ODS_SOURCE')
-    output_file = os.getenv('CSV_SOURCE')
-    CHECK_FOR_DUPLICATES = 'left'
+    output_file = os.getenv('CSV_PREFIX') + '_sheet_' + args.sheetname + '.csv'
+    check_col = args.chk
     
-    convert_ods_to_csv(input_file, output_file, CHECK_FOR_DUPLICATES)
+    convert_ods_to_csv(
+        input_file=input_file, 
+        output_file=output_file, 
+        check_col=check_col,
+        sheet_name=args.sheetname
+        )
